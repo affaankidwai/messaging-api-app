7977ac9f0bf4658f21df9ac9713aabf89a6ec2e1
Affaan Kidwai <kidwaiaffaan@gmail.com>
2024-07-12 15:42:16 +0530

    try

--- src/main/java/com/training/messaging/MessagingApiAppApplication.java
+++ src/main/java/com/training/messaging/MessagingApiAppApplication.java
@@ -2,12 +2,16 @@ package com.training.messaging;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
 
 @SpringBootApplication
+@RestController
 public class MessagingApiAppApplication {
 	
-	public String printGreetings() {
-		return "Hi Oracle";
+	@GetMapping("enjoyWeekend")  
+	public String weekend() {
+		return "Saturday and Sunday";
 	}
 
 	public static void main(String[] args) {
